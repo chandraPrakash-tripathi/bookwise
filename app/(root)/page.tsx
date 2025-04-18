28e7e20ae9e2abfc8abe9728f30620858876c2d8
CP0070 <tripathi.cp07@gmail.com>
2025-04-18 17:54:17 +0530

    drizzle setup plus neon setup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,13 @@
 import BookList from "@/components/BookList";
 import BookOverview from "@/components/BookOverview";
 import { sampleBooks } from "@/constants";
+import { db } from "@/db/drizzle";
+import { users } from "@/db/schema";
 import React from "react";
-const page = () => {
+const page = async() => {
+  const result = await db.select().from(users)
+  console.log("result", result);
+  
   return (
     <>
       <BookOverview {...sampleBooks[0]} />
