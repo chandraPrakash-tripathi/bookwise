c63e1aaa3a9dcd49625f603d35bd92afa4c1796d
CP0070 <tripathi.cp07@gmail.com>
2025-04-19 19:02:57 +0530

    workflow completed-added

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,17 +3,44 @@ import "../globals.css";
 import Header from "@/components/Header";
 import { auth } from "@/auth";
 import { redirect } from "next/navigation";
+import { after } from "next/server";
+import { db } from "@/db/drizzle";
+import { users } from "@/db/schema";
+import { eq } from "drizzle-orm";
 const Layout = async ({ children }: { children: ReactNode }) => {
+  const session = await auth();
+  if (!session) {
+    redirect("/sign-in");
+  }
+  if (!session.user || !session.user.id) {
+    redirect("/sign-in");
+  }
 
-  const session = await auth()
-    if(!session){
-      redirect("/sign-in")
-    }
+  // Now TypeScript knows userId is a string
+  const userId = session.user.id;
+
+  //get the single user and see if the lasta ctivity date is today
+  const user = await db
+    .select()
+    .from(users)
+    .where(eq(users.id, userId))
+    .limit(1);
+
+  if (user[0].lastActivityDate === new Date().toISOString().slice(0, 10))
+    return;
+
+  after(async () => {
+    if (!session?.user?.id) return;
+    await db
+      .update(users)
+      .set({ lastActivityDate: new Date().toString().slice(0, 10) })
+      .where(eq(users.id, session?.user?.id));
+  });
+
   return (
-    <main  className="root-container flex min-h-screen flex-1 flex-col bg-gradient-to-br from-gray-900 via-blue-950 to-black bg-cover bg-top px-5 xs:px-10 md:px-16 text-white">
-
+    <main className="root-container flex min-h-screen flex-1 flex-col bg-gradient-to-br from-gray-900 via-blue-950 to-black bg-cover bg-top px-5 xs:px-10 md:px-16 text-white">
       <div className="mx-auto max-w-7xl">
-        <Header session  ={session}/>
+        <Header session={session} />
         <div className="mt-20 pb-20"> {children}</div>
       </div>
     </main>
--- app/api/workflows/onboarding/route.ts
+++ app/api/workflows/onboarding/route.ts
@@ -1,16 +1,52 @@
+import { db } from "@/db/drizzle";
+import { users } from "@/db/schema";
 import { serve } from "@upstash/workflow/nextjs";
+import { eq } from "drizzle-orm";
+import { sendEmail } from "@/lib/workflow";
 
+type UserState = "non-active" | "active";
 type InitialData = {
-  email: string;
+  email: string;fullName: string;
+};
+const ONE_DAY_IN_MS = 24 * 60 * 60 * 1000;
+const THREE_DAYS_IN_MS = 3 * ONE_DAY_IN_MS;
+const THIRTY_DAYS_IN_MS = 30 * ONE_DAY_IN_MS;
+
+//check lastactivity state and dervie the user state
+const getUserState = async (email: string): Promise<UserState> => {
+  const user = await db
+    .select()
+    .from(users)
+    .where(eq(users.email, email))
+    .limit(1);
+
+  if (user.length === 0) return "non-active";
+
+  const lastActivityDate = new Date(user[0].lastActivityDate!);
+  const now = new Date();
+  const timeDifference = now.getTime() - lastActivityDate.getTime();
+
+  if (
+    timeDifference > THREE_DAYS_IN_MS &&
+    timeDifference <= THIRTY_DAYS_IN_MS
+  ) {
+    return "non-active";
+  }
+
+  return "active";
 };
 
 export const { POST } = serve<InitialData>(async (context) => {
-  const { email } = context.requestPayload;
+  const { email, fullName } = context.requestPayload;
 
   //start by sending a newly signed-up user a welcome email
   //this is a workflow that will be run in the background
   await context.run("new-signup", async () => {
-    await sendEmail("Welcome to the platform", email);
+    await sendEmail({
+      email,
+      subject: "Welcome to Bookwise!",
+      message:`Welcome ${fullName} to Bookwise! We are excited to have you on board.`
+    });
   });
 
   //Initial Waiting Period
@@ -21,31 +57,27 @@ export const { POST } = serve<InitialData>(async (context) => {
   //We enter an infinite loop to periodically (every month) check the user’s engagement level with our platform and send appropriate emails:
   while (true) {
     const state = await context.run("check-user-state", async () => {
-      return await getUserState();
+      return await getUserState(email);
     });
 
     if (state === "non-active") {
       await context.run("send-email-non-active", async () => {
-        await sendEmail("Email to non-active users", email);
+        await sendEmail({
+          email,
+          subject: "You've been inactive for a while",
+          message: `Hi ${fullName}, we noticed you haven't been active for a while. We miss you! Come back and check out the latest features on Bookwise.`,
+        });
       });
     } else if (state === "active") {
       await context.run("send-email-active", async () => {
-        await sendEmail("Send newsletter to active users", email);
+        await sendEmail({
+          email,
+          message: `Hi ${fullName}, thanks for being an active user! We appreciate your engagement with Bookwise. If you have any feedback, feel free to reach out.`,
+          subject: "Thanks for being an active user!",
+        });
       });
     }
 
     await context.sleep("wait-for-1-month", 60 * 60 * 24 * 30);
   }
 });
-
-async function sendEmail(message: string, email: string) {
-  // Implement email sending logic here
-  console.log(`Sending ${message} email to ${email}`);
-}
-
-type UserState = "non-active" | "active";
-
-const getUserState = async (): Promise<UserState> => {
-  // Implement user state logic here
-  return "non-active";
-};
