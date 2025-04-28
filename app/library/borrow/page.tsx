f8aeff28093fd617868833b8bea093f82ace0110
CP0070 <tripathi.cp07@gmail.com>
2025-04-28 15:13:34 +0530

    changes

--- app/library/borrow/page.tsx
+++ app/library/borrow/page.tsx
@@ -1,13 +1,41 @@
-import { auth } from '@/auth'
+import BorrowMembers from '@/components/library/BorrowMembers'
+import { db } from '@/db/drizzle'
+import { books, borrowRecords, users } from '@/db/schema'
+import {  eq, isNotNull } from 'drizzle-orm'
 import React from 'react'
 
 const page = async() => {
+// Fetching borrowed books data from the database
+// and joining with users and books tables to get additional information
 
-  const session = await auth()
+  const borrowedBooks = await db
+  .select({
+    id: borrowRecords.id,
+    
+    userId: borrowRecords.userId,
+    borrowerName: users.fullName,  // Added borrower name from users table
+    
+    bookId: borrowRecords.bookId,
+    bookTitle: books.title,  // Added book title from books table
+    bookIsbn: books.isbn,
+    
+    status: borrowRecords.status,
+    borrowDate: borrowRecords.borrowDate,
+    dueDate: borrowRecords.dueDate,
+  })
+  .from(borrowRecords)
+  .innerJoin(users, eq(borrowRecords.userId, users.id))  // Join with users table
+  .innerJoin(books, eq(borrowRecords.bookId, books.id))  // Join with books table
+  .where(isNotNull(borrowRecords.borrowDate))  // Only books with a borrowDate (borrowed books)
+  .orderBy(borrowRecords.borrowDate);
   
+              
+  
+
+
   return (
     <div>
-      sfs
+      <BorrowMembers borrowedBooks={borrowedBooks} />
     </div>
   )
 }
--- components/library/HeaderLib.tsx
+++ components/library/HeaderLib.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Header = ({ session }: { session: Session }) => {
   return (
-    <header className="library-header flex lg:items-end items-start justify-between lg:flex-row flex-col gap-5 sm:mb-10 mb-5 ml-4">
+    <header className="library-header flex lg:items-end items-start justify-between lg:flex-row flex-col gap-5 sm:mb-10 mb-5 shadow-2xl bg-gray-300 p-4 rounded-lg">
       <div>
         <h2 className="text-2xl font-semibold text-black">
           {session?.user?.name}
