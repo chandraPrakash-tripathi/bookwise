639d8fd3e6f1f004764f71a401ce6c1f96eae53a
CP0070 <tripathi.cp07@gmail.com>
2025-05-06 23:15:24 +0530

    ui-changes

--- app/(root)/book/[id]/page.tsx
+++ app/(root)/book/[id]/page.tsx
@@ -9,7 +9,7 @@ import React from 'react'
 
 const page = async({params}:{params:Promise<{id:string,}>}) => {
     const id = (await params).id
-    const session  =await auth()
+    const session = await auth()
     console.log(id)
 
     const [bookDetails] = await db.select().from(books).where(eq(books.id, id)).limit(1)
@@ -31,21 +31,22 @@ const page = async({params}:{params:Promise<{id:string,}>}) => {
 
     return (
     <>
-    <BookOverview {...bookWithDefaults} userId ={session?.user?.id as string}/>
+    <BookOverview {...bookWithDefaults} userId={session?.user?.id as string}/>
      <div className='book-details lg:mt-36 mt-16 mb-20 flex flex-col gap-16 lg:flex-row'>
       <div className='flex-[1.5]'>
         <section className='flex flex-col gap-7'>
           <h3 className='text-semibold text-2xl text-yellow-200'>
             Video
-            <BookVideo videoUrl={bookDetails.videoUrl} />
           </h3>
+          <BookVideo videoUrl={bookDetails.videoUrl} />
+          
           <section className='mt-10 flex flex-col gap-7'>
             <h3>Summary</h3>
             
             <div className='space-y-5 text-xl text-white'>
-              <p>{bookDetails.summary.split('\n').map((line,i)=>(
+              {bookDetails.summary.split('\n').map((line, i) => (
                 <p key={i}>{line}</p>
-              ))}</p>
+              ))}
             </div>
           </section>
 
